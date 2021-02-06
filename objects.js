29e171ecc64667ed8303b86a4a4a0a4bfb3d6019
Kyle Samuel <ksamuel92@gmail.com>
2021-02-06 17:37:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,11 @@
 var playlist = {'The 1975': Someone Else, 'MF DOOM': Accordion, 'Miles Davis': Four}
 
 function updatePlaylist (playlist, artist, song) {
-   Object.assign({}, playlist:)
+   Object.assign({}, playlist, {artist: song});
+   return playlist;
+}
+
+function removeFromPlaylist (playlist, artist) {
+  delete playlist.artist
+  return playlist
 }
