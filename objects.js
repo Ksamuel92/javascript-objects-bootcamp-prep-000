005bdec40b1225ee327638d289bccf30b21df459
Kyle Samuel <ksamuel92@gmail.com>
2021-02-06 17:47:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-const playlist = {'The 1975': 'Someone Else',};
+const playlist = {"The 1975": Me};
 
 function updatePlaylist (playlist, artistName, songTitle) {
    Object.assign({}, playlist, { [artistName]:songTitle});
